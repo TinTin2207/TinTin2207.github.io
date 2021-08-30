c1520077b903cb81169ca4a5f8ea47cc49b796b7
TinTin <70331383+TinTin2207@users.noreply.github.com>
2021-08-30 14:56:09 +0700

    Update transition.js

--- js/transition.js
+++ js/transition.js
@@ -1,4 +1,8 @@
 window.onload = function() {
     const transition = document.querySelector('.transition');
     transition.classList.add("slide");
+    
+    setTimeout(() =>  {
+        transition.classList.remove("slide");
+    }, 900)
 }
